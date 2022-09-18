4ebbbb9de0b15131c8aace25ac9e37d70050e214
hujiajun <123@hjj.com>
2022-09-18 19:04:21 +0800

    图片预览组件

--- company/admin/components.d.ts
+++ company/admin/components.d.ts
@@ -20,6 +20,8 @@ declare module '@vue/runtime-core' {
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElImage: typeof import('element-plus/es')['ElImage']
+    ElImageViewer: typeof import('element-plus/es')['ElImageViewer']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
--- company/admin/src/views/upload/index.vue
+++ company/admin/src/views/upload/index.vue
@@ -1,3 +1,40 @@
 <template>
-  <div>上传文件</div>
+  <div>
+    <el-button @click="imgPreview">图片预览</el-button>
+    <el-image-viewer
+      v-if="imgView.showView"
+      :url-list="srcList"
+      :initial-index="imgView.index"
+      @close="closeViewer"
+      @switch="changeViewer"
+    ></el-image-viewer>
+  </div>
 </template>
+
+<script lang="ts" setup name="upload">
+import { reactive, ref } from 'vue'
+const imgView = reactive({
+  index: 5,
+  showView: false
+})
+const srcList = ref([
+  'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
+  'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+  'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
+  'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
+  'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
+  'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
+  'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg',
+])
+
+const imgPreview = () => {
+  // elImageRef.value.onclick()
+  imgView.showView = true
+}
+const closeViewer = () => {
+  imgView.showView = false
+}
+const changeViewer = (i: number) => {
+  console.log(i)
+}
+</script>
