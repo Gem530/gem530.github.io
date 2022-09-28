dc5f409dd7ae53d1c5e0f8b241bf6204cd662095
hujiajun <123@hjj.com>
2022-09-28 20:03:48 +0800

    admin-上传图片优化

--- company/admin/src/components/g-img-upload/index.vue
+++ company/admin/src/components/g-img-upload/index.vue
@@ -26,18 +26,36 @@
 <script lang="ts" setup name="g-upload">
 import { getItem } from '@/utils/storage'
 import { UploadFile, ElMessage } from 'element-plus'
-import { ref, useAttrs, reactive, defineProps, withDefaults } from 'vue'
+import { ref, watch, useAttrs, reactive, defineProps, withDefaults } from 'vue'
 interface data {
   fileTo: string
 }
-
+interface response {
+  msg: string,
+  code: number
+}
+type UploadFiles = Partial<UploadFile>[]
+interface upload extends UploadFile {
+  response: response
+}
 const attrs = useAttrs()
 const props = withDefaults(defineProps<{
   data?: data,
-  action?: string
+  action?: string,
+  modelValue?: UploadFiles
 }>(), {
   data: () => ({ fileTo: '' }),
-  action: "http://54.151.135.104:9001/upload/img"
+  action: "http://54.151.135.104:9001/upload/img",
+  modelValue: () => [{
+    uid: 1,
+    url: 'https://fa-file-test.s3.ap-southeast-1.amazonaws.com/uploadfile/About us备份@2x.png'
+  }, {
+    uid: 2,
+    url: 'https://fa-file-test.s3.ap-southeast-1.amazonaws.com/uploadfile/Analysis备份@2x.png'
+  }, {
+    uid: 3,
+    url: 'https://fa-file-test.s3.ap-southeast-1.amazonaws.com/uploadfile/Bill备份@2x.png'
+  }]
 })
 
 const headers = ref({ Authorization: getItem('token') })
@@ -45,41 +63,36 @@ const imgView = reactive({
   index: 5,
   showView: false
 })
-const fileList = ref<Partial<UploadFile>[]>([])
-fileList.value = [{
-  uid: 1,
-  url: 'https://fa-file-test.s3.ap-southeast-1.amazonaws.com/uploadfile/About us备份@2x.png'
-}, {
-  uid: 2,
-  url: 'https://fa-file-test.s3.ap-southeast-1.amazonaws.com/uploadfile/Analysis备份@2x.png'
-}, {
-  uid: 3,
-  url: 'https://fa-file-test.s3.ap-southeast-1.amazonaws.com/uploadfile/Bill备份@2x.png'
-}]
+const fileList = ref<UploadFiles>([])
+watch(() => props.modelValue, (newVal: UploadFiles) => {
+  if (newVal && newVal.length > 0) {
+    fileList.value = newVal
+  }
+}, { deep: true, immediate: true })
 
+const handleChange = (file: upload) => {
+  console.log('change', file)
+  if (file.status === 'success') {
+    if (file.response.code === 200) {
+      ElMessage.success(`${file.name} 上传成功`)
+    } else {
+      ElMessage.error(`${file.response.msg}`)
+    }
+  } else if (file.status === 'fail') {
+    ElMessage.error(`${file.name} 上传失败`)
+  }
+}
+const handleRemove = (file: UploadFile) => {
+  console.log('删除', file)
+}
+
+// 图片预览方法
 const handlePictureCardPreview = (file: UploadFile) => {
   const i = fileList.value.findIndex((v: Partial<UploadFile>) => { return v.uid === file.uid })
   // console.log('预览', file, i)
   imgView.index = i
   imgView.showView = true
 }
-const handleChange = (file: any, i: any) => {
-  console.log('change', file, i)
-  // if (file.file.status === 'done') {
-  //   if (file.file.response.success) {
-  //     ElMessage.success(`${file.file.name} 上传成功`)
-  //   } else {
-  //     ElMessage.error(`${file.file.response.message}`)
-  //   }
-  // } else if (file.file.status === 'error') {
-  //   ElMessage.error(`${file.file.name} 上传失败`)
-  // }
-}
-const handleRemove = () => {
-  console.log('删除')
-}
-
-// 图片预览方法
 const closeViewer = () => {
   imgView.showView = false
 }
