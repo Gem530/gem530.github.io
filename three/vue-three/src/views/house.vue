251fcfd62de1d4438427b032aa3ed960e0a0a060
hujiajun <123@hjj.com>
2023-02-09 19:40:45 +0800

    three-新增house切换功能

--- three/vue-three/src/views/house.vue
+++ three/vue-three/src/views/house.vue
@@ -1,5 +1,8 @@
 <template>
-  <div ref="houseRef"></div>
+  <div>
+    <button class="fixed-btn" @click="changeHouse">切换</button>
+    <div ref="houseRef"></div>
+  </div>
 </template>
 
 <script lang="ts" name="House" setup>
@@ -8,73 +11,96 @@ import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { ref, watch, reactive, onMounted, computed } from 'vue'
-console.log(THREE)
 
 const houseRef = ref()
+const changeFlag = ref(true)
 
-onMounted(() => {
-  var scene = new THREE.Scene();
-  var box = new THREE.SphereGeometry(25, 50, 50);
-  var material = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    side: THREE.BackSide,
-  });
-  var mesh = new THREE.Mesh(box, material);
-  scene.add(mesh);
+var scene = new THREE.Scene(); // 创建场景
+var box = new THREE.SphereGeometry(25, 50, 50); // 创建几何体
+var material = new THREE.MeshBasicMaterial({ // 创建材质对象
+  color: 0xffffff,
+  side: THREE.BackSide,
+});
+var mesh = new THREE.Mesh(box, material); // 创建网格模型对象Mesh
+scene.add(mesh); // 添加到场景中
 
-  var textureLoader = new THREE.TextureLoader();
-  var listener = new THREE.AudioListener();
-  var audio = new THREE.Audio(listener);
-  var texture = textureLoader.load(
-    "../../public/00125.jpg",
-    function (obj: any) {
-      // console.log(vm.loading);
-      // vm.loading.close();
-      var audioLoader = new THREE.AudioLoader();
-      audioLoader.load("../../public/琵琶语.mp3", function (AudioBuffer: any) {
-        audio.setBuffer(AudioBuffer);
-        audio.setLoop(true);
-        audio.setVolume(0.3);
-        audio.play();
-      });
-      render();
-    }
+var textureLoader = new THREE.TextureLoader(); // 创建纹理
+var listener = new THREE.AudioListener();
+var audio = new THREE.Audio(listener);
+var texture = textureLoader.load( // 纹理加载
+  "../../public/00125.jpg",
+  function (obj: any) {
+    // console.log(vm.loading);
+    // vm.loading.close();
+    var audioLoader = new THREE.AudioLoader();
+    audioLoader.load("../../public/琵琶语.mp3", function (AudioBuffer: any) {
+      audio.setBuffer(AudioBuffer);
+      audio.setLoop(true);
+      audio.setVolume(0.3);
+      audio.play();
+    });
+    render();
+  }
+);
+mesh.material.map = texture; // 修改模型纹理
+var width = window.innerWidth;
+var height = window.innerHeight;
+var k = width / height;
+
+// 创建相机
+var camera = new THREE.PerspectiveCamera(60, width / height, 1, 1000);
+camera.zoom = 1;
+camera.updateProjectionMatrix();
+camera.position.set(-0.87, 0.03, 0.4);
+camera.lookAt(scene.position);
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height);
+// document.body.appendChild(renderer.domElement);
+var clock = new THREE.Clock();
+var FPS = 30;
+var refreshTime = 1 / FPS;
+var timeS = 0;
+function render() {
+  requestAnimationFrame(render);
+  var renderingInterval = clock.getDelta();
+  timeS = timeS + renderingInterval;
+  if (timeS > refreshTime) {
+    renderer.render(scene, camera);
+    // if (vm.rotateBoool) {
+      mesh.rotateY(0.002);
+    // }
+    timeS = 0;
+  }
+}
+render();
+
+// 鼠标事件 移动，缩放
+var controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', render); // 监听鼠标、键盘事件
+// controls.enablePan = false;
+// var styleObjArr = path();
+
+function changeHouse () {
+  changeFlag.value = !changeFlag.value
+  let url = changeFlag.value ? '../../public/00125.jpg' : '../../public/0011.jpg'
+  texture = textureLoader.load(
+    url,
+    render()
   );
   mesh.material.map = texture;
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  var k = width / height;
+  render()
+}
 
-  var camera = new THREE.PerspectiveCamera(60, width / height, 1, 1000);
-  camera.zoom = 1;
-  camera.updateProjectionMatrix();
-  camera.position.set(-0.87, 0.03, 0.4);
-  camera.lookAt(scene.position);
-
-  var renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(width, height);
-  // document.body.appendChild(renderer.domElement);
+onMounted(() => {
   houseRef.value.appendChild(renderer.domElement); // 元素中插入canvas对象
-  var clock = new THREE.Clock();
-  var FPS = 30;
-  var refreshTime = 1 / FPS;
-  var timeS = 0;
-  function render() {
-    requestAnimationFrame(render);
-    var renderingInterval = clock.getDelta();
-    timeS = timeS + renderingInterval;
-    if (timeS > refreshTime) {
-      renderer.render(scene, camera);
-      // if (vm.rotateBoool) {
-        mesh.rotateY(0.002);
-      // }
-      timeS = 0;
-    }
-  }
-  render();
-  var controls = new OrbitControls(camera, renderer.domElement);
-  controls.addEventListener('change', render); // 监听鼠标、键盘事件
-  // controls.enablePan = false;
-  // var styleObjArr = path();
 })
 </script>
+
+<style lang="scss" scoped>
+.fixed-btn {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
