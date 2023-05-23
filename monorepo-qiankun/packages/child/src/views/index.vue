be26df01f97d0dccf23a336d1b98321e470b85cb
hujiajun <123@hjj.com>
2023-05-23 15:30:48 +0800

    测试添加sass依赖

--- monorepo-qiankun/packages/child/src/views/index.vue
+++ monorepo-qiankun/packages/child/src/views/index.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
+  <div class="home">
     <h3>子项目</h3>
     <router-link :to="'/'">跳转=>main</router-link>
     <br />
     <router-link :to="'/admin/login'">跳转=>admin</router-link>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
